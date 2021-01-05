e996cd8c6252be5e144e761d9dc1d06c48177668
Alexandre De Carli <alexandredecarli123@gmail.com>
2021-01-04 22:59:41 -0300

    Create datafeed.js

---
+++ datafeed.js
@@ -0,0 +1,20 @@
+    class Datafeed {
+    onReady = function (cb) {
+
+    };
+
+
+    resolveSymbol(symbolName, onSymbolResolvedCallback, onResolveErrorCallback) {
+
+    }
+
+    getBars(symbolInfo, resolution, from, to, onHistoryCallback, onErrorCallback, firstDataRequest) {
+
+    }
+
+    subscribeBars(symbolInfo, resolution, onRealtimeCallback, subscriberUID, onResetCacheNeededCallback) {
+    }
+
+    unsubscribeBars(subscriberUID) {
+    }
+}
